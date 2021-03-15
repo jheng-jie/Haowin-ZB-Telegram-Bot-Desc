12f6502f9ce07944f62f3668005ba61c06959e7d
jie <jie@haowin.org>
2021-03-15 09:51:46 +0800

    feat: lazy load

--- src/router.ts
+++ src/router.ts
@@ -1,47 +1,49 @@
-import { createWebHashHistory, createRouter } from "vue-router"
+import { defineAsyncComponent } from "vue"
+import { createWebHashHistory, createRouter, RouteRecordRaw } from "vue-router"
 
 // page
-import iOS_Build from "./page/iOSBuild.vue"
-import Android_Build from "./page/AndroidBuild.vue"
-import Param from "./page/Param.vue"
 import Home from "./page/Home.vue"
 
+// paths
+const routes: Array<RouteRecordRaw> = [
+  {
+    path: "/",
+    name: "HOME",
+    meta: {
+      keepAlive: true
+    },
+    component: Home
+  },
+  {
+    path: "/ios-build",
+    name: "IOS_BUILD",
+    meta: {
+      keepAlive: true
+    },
+    component: defineAsyncComponent(() => import("./page/iOSBuild.vue"))
+  },
+  {
+    path: "/android-build",
+    name: "ANDROID_BUILD",
+    meta: {
+      keepAlive: true
+    },
+    component: defineAsyncComponent(() => import("./page/AndroidBuild.vue"))
+  },
+  {
+    path: "/param",
+    name: "QA",
+    meta: {
+      keepAlive: true
+    },
+    component: defineAsyncComponent(() => import("./page/Param.vue"))
+  }
+]
+
+// router
 const router = createRouter({
   history: createWebHashHistory(),
-  routes: [
-    {
-      path: "/",
-      name: "HOME",
-      meta: {
-        keepAlive: true
-      },
-      component: Home
-    },
-    {
-      path: "/ios-build",
-      name: "IOS_BUILD",
-      meta: {
-        keepAlive: true
-      },
-      component: iOS_Build
-    },
-    {
-      path: "/android-build",
-      name: "ANDROID_BUILD",
-      meta: {
-        keepAlive: true
-      },
-      component: Android_Build
-    },
-    {
-      path: "/param",
-      name: "QA",
-      meta: {
-        keepAlive: true
-      },
-      component: Param
-    }
-  ]
+  routes
 })
 
 export default router
