d6297abce64a75581462c835049b41b00c1ceb02
jie <jie@haowin.org>
2021-03-17 18:14:36 +0800

    feat: deploy script

--- script/deploy.js
+++ script/deploy.js
@@ -1,8 +1,17 @@
-const FileSystem = require("fs")
+const FileSystem = require("fs").promises
+const ncp = require('ncp').ncp;
 const Path = require("path")
-
+const DistPath = Path.resolve(__dirname, "../dist")
 const RemovePath = Path.resolve(__dirname, "../docs")
 
-const Rmdir = async function () {
-
+async function init() {
+  await FileSystem.rmdir(RemovePath, { recursive: true }).then(() => console.log(`${RemovePath} clear success.`))
+  ncp(DistPath, RemovePath, function (err) {
+    if (err) {
+      return console.error(err);
+    }
+    console.log('done!');
+  });
 }
+
+init().catch(e => console.error(e))
