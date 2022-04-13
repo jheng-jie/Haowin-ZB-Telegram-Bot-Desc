7fb3a1f985b3ce2ed5b686b988946e332e6cbf0a
jie <jie@haowin.org>
2022-04-13 09:15:00 +0800

    feat: rebuild

--- vite.config.ts
+++ vite.config.ts
@@ -4,12 +4,15 @@ const { resolve } = require("path")
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "/Haowin-ZB-Telegram-Bot-Desc/",
+  base: "vite-tg-build-bot/",
   plugins: [vue()],
   resolve: {
     extensions: [".ts", ".tsx", ".js"],
     alias: {
       "/@": resolve(__dirname, "./src")
     }
+  },
+  build: {
+    outDir: "docs"
   }
 })
